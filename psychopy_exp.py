21b7dd8e80039af4d2bbbbd1a7ddf4fd0c07b3c6
CruX UCLA <cruxneurotech@g.ucla.edu>
2024-02-04 14:57:23 -0800

    removing weird code

--- psychopy_exp.py
+++ psychopy_exp.py
@@ -9,40 +9,6 @@ REFRESH_RATE = 60
 # Create a window
 win = visual.Window(size=(800, 600), monitor='testMonitor', color='black')
 
-if ((showVideo !== undefined)) {
-let src = 'youtube.html';
-params = {};  // Results added here after form is submitted
-continue_routine = true; // Routines can't be ended from within Begin Routine
-$(document).ready(function() {
-    // Add custom contents from html file using an iframe:
-    $('body').append('<div id="iframe-o" style="visibility: hidden; position: relative; display: table; margin: auto;"><div id="iframe-m" style="display: table-cell; vertical-align: middle;"><div id="iframe-i" style="display: inline-block; width:100%; overflow-y: auto; overflow-x: hidden;"><iframe id="iframe" src="'+src+'" frameborder=0 style="width: 100%"></iframe></div></div></div>');
-
-    $('#iframe').on('load',function(iframe){
-        // Auto-adjust iframe size:
-        $(this).contents().find('html').css({ 'display': 'table', 'width': '100%', 'overflow-x': 'hidden' });
-        $('#iframe-o').height($(window).height()-20, true);
-        $('#iframe-m').width($(this).contents().find('html').width()+20);
-        $('#iframe-i').height ( Math.min ( $(this).contents().find('html').height()+20, $(window).height()-20 ), true );
-        $(this).height($(this).contents().find('html').height());
-        $('#iframe-o').css('visibility','visible');
-
-        // If iframe contains a form, then capture its output:
-        $(this).contents().find('form').on('submit',function(e){
-            e.preventDefault();
-            $.each($(this).serializeArray(),function(i, param){
-                params[param.name] = param.value;
-                psychoJS.experiment.addData(param.name, param.value);
-            });
-            console.log ( 'DEBUG:FRM', params );
-            // Remove iframe and continue to next routine when done:
-            $('#iframe-o').remove();
-            continue_routine = false;
-        });
-    });
-});
-//$('#iframe').attr( 'src', function(i,val){ return val;} );
-}
-
 # A light green text
 stim = visual.TextStim(win, 'Click any key to Continue', color=(1, 1, 1), colorSpace='rgb')
 stim.colorSpace = 'rgb255'
