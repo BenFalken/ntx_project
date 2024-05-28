8875a9e1585592ba6378ac08073020013764daf0
sandrakaron <sandrahkaron@gmail.com>
2024-05-28 16:21:33 -0700

    Added 3D GUI graph

--- GUIs/demonstration_GUI_final_3D.py
+++ GUIs/demonstration_GUI_final_3D.py
@@ -5,31 +5,39 @@ import time
 
 def update_point(scatter, coords):
     # Update the offsets for the scatter plot
-    scatter._offsets3d = (coords[0], coords[1], coords[2])
+    scatter._offsets3d = (np.array([coords[0]]), np.array([coords[1]]), np.array([coords[2]]))
     plt.draw()
 
 def generate_random_coordinates():
     return np.random.rand(3) * 10  # Generate random coordinates within [0, 10) range
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+def main():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-# Initial coordinates
-initial_coords = generate_random_coordinates()
-scatter = ax.scatter(initial_coords[0], initial_coords[1], initial_coords[2], color='b')
+    # Initial coordinates
+    initial_coords = generate_random_coordinates()
+    scatter = ax.scatter(initial_coords[0], initial_coords[1], initial_coords[2], color='b')
 
-ax.set_xlim([0, 10])
-ax.set_ylim([0, 10])
-ax.set_zlim([0, 10])
+    ax.set_xlabel('Valence')
+    ax.set_ylabel('Arousal')
+    ax.set_zlabel('Dominance')
 
-plt.ion()  # Turn on interactive mode
-plt.show()
 
-try:
-    while True:
-        new_coords = generate_random_coordinates()
-        update_point(scatter, new_coords)
-        plt.pause(3)  # Pause for 3 seconds
-        print(new_coords)
-except KeyboardInterrupt:
-    print("Plotting stopped.")
+    ax.set_xlim([0, 10])
+    ax.set_ylim([0, 10])
+    ax.set_zlim([0, 10])
+
+    plt.ion()  # Turn on interactive mode
+    plt.show()
+
+    try:
+        while True:
+            new_coords = generate_random_coordinates()
+            update_point(scatter, new_coords)
+            plt.pause(3)  # Pause for 3 seconds
+    except KeyboardInterrupt:
+        print("Plotting stopped.")
+
+if __name__ == "__main__":
+    main()
